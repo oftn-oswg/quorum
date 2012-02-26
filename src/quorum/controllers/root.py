de69fe9defdc88bcaeb2fe58a3626f557d7e9070
Alice Bevan-McGregor <alice@gothcandy.com>
2012-02-25 19:18:19 -0500

    Additional HTML updates and extended the documentation with installation and runtime instructions.

--- src/quorum/controllers/root.py
+++ src/quorum/controllers/root.py
@@ -12,7 +12,7 @@ from quorum.controllers.auth import AuthenticationMixIn
 class RootController(Controller, AuthenticationMixIn):
     def index(self):
         """Present anonymous users with a welcome page, redirect authenticated users to the issue list."""
-        if web.auth.authenticated:
-            raise web.core.http.HTTPFound(location='/issues')
+        #if web.auth.authenticated:
+        #    raise web.core.http.HTTPFound(location='/issues/')
         
         return 'quorum.templates.master', dict()
